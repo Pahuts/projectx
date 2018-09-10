364255336586c1e16dbd9332e5d7557b15088090
Pahuts <luispahutan@gmail.com>
2018-09-10 15:52:03 +0800

    stars3

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,10 @@ module Projectx
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
-
+    config.to_prepare do
+      Devise::SessionsController.layout "home"
+    end
+    config.assets.initialize_on_precompile = false
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
