916d5e9f0e52808408454081434272e9679e3a7a
Pahuts <luispahutan@gmail.com>
2018-09-17 14:22:37 +0800

    environment rb configured host

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,6 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+config.action_mailer.default_url_options = { :host => 'cryptic-ocean-69494.herokuapp.com' }
+
